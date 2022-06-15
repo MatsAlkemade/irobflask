589b5863f57ebe068e100c36df84ca0cfd986ecf
MatsAlkemade <matsalkemade@gmail.com>
2022-06-15 10:22:16 +0200

    init

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask,jsonify,redirect,render_template,request,redirect,make_response
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    productcode = db.Column(db.String(200), nullable=False)
+    product_name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now(pytz.timezone("Europe/Amsterdam")))
+
+    def __repr__(self):
+        return '<Product %r>' % self.id
+    
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    template_name = db.Column(db.String(200))
+    accuracy = db.Column(db.Integer)
+    barcode = db.Column(db.String(200))
+    date_created = db.Column(db.DateTime)
+    
+    def __repr__(self):
+        return '<Match %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET', 'DELETE'])
+def index():
+    if request.method == 'POST':
+        productcode = request.form['productcode']
+        product_name = request.form['product_name']
+        new_product = Product(productcode=productcode, product_name=product_name)
+        try:
+            db.session.add(new_product)
+            db.session.commit()
+            return jsonify({
+                'id' : new_product.id,
+                'productcode' : new_product.productcode,
+                'product_name' : new_product.product_name,
+                'date_created' : new_product.date_created.strftime("%Y") + "-" + new_product.date_created.strftime("%m") + "-" + new_product.date_created.strftime("%d")
+            })
+               
+        except:
+            return "Er is iets fout gegaan"
+    else:
+        products = Product.query.order_by(Product.date_created).all()
+        return render_template('index.html', products=products)
+
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete(id):
+    product_to_delete = Product.query.get_or_404(id)
+
+    try:
+        db.session.delete(product_to_delete)
+        db.session.commit()
+        return jsonify({
+            'id' : id
+        })
+    except:
+        return 'Er is iets foutgegaan'
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ static/js/main.js
@@ -0,0 +1,121 @@
+document.getElementById('qr-reader').style.width = "360px";
+
+const html5QrcodeScanner = new Html5Qrcode("qr-reader");
+const config = {
+  fps: 10,
+  aspectRatio: 1.0,
+};
+
+var scanCooldown = false;
+const cooldownBanner = document.getElementById("cooldownBanner");
+
+const bcModal = document.getElementById("bcModal");
+const closeBcModalBtn = document.getElementById("closeBcModalBtn");
+const bcSaveBtn = document.getElementById("bcSaveBtn");
+
+function qrCodeSuccessCallback(decodedText, decodedResult){
+  if(scanCooldown == false){
+    scanCooldown = true;
+    cooldownBanner.style.display = "flex";
+    openBarcodeModal();
+    document.getElementById("barcodeResult").innerHTML = decodedResult.result.text;
+    document.getElementById("productcode").value = decodedResult.result.text;
+  }
+}
+
+function deleteProduct(id){
+  req = $.ajax({
+    url : '/delete/' + id,
+    type : 'DELETE',
+    data : { id : id }
+  });
+
+  req.done(function(data){
+    const tb = document.getElementById("productlist__table__body");
+    const tr = document.getElementById(data.id);
+    tb.removeChild(tr);
+  });
+}
+
+function openBarcodeModal(){
+    bcModal.style.display = "block";
+}
+
+function closeBarcodeModal(){
+    document.getElementById("productcode").value = "";
+    document.getElementById("product_name").value = "";
+    bcModal.style.display = "none";
+    
+    setTimeout(() => {
+      scanCooldown = false;
+      cooldownBanner.style.display = "none";
+    }, 1500);
+}
+
+bcSaveBtn.onclick = function() {
+  if(document.getElementById("productcode").value == "" || document.getElementById("product_name").value == ""){
+    document.getElementById("bcRequired").style.color = "rgba(255,0,0,1)";
+  } else {
+    saveBarcode();
+  }
+  setTimeout(() => {
+    document.getElementById("bcRequired").style.color = "rgba(255,0,0,0)";
+  }, 1000);
+}
+
+function saveBarcode() {
+    var productcode = document.getElementById("productcode").value;
+    var product_name = document.getElementById("product_name").value;
+    
+    req = $.ajax({
+      url : '/',
+      type : 'POST',
+      data : {
+        productcode : productcode,
+        product_name : product_name,
+        }
+    });
+  
+    req.done(function(data){
+      setTimeout(() => {
+        const productList = document.getElementById("productlist__table__body");
+      
+        const row = productList.insertRow(-1);
+        row.setAttribute("id",data.id);
+        
+        const cell1 = row.insertCell(0);
+        cell1.innerHTML = data.product_name;
+    
+        const cell2 = row.insertCell(1);
+        cell2.innerHTML = data.productcode;
+    
+        const cell3 = row.insertCell(2);
+        cell3.innerHTML = data.date_created;
+    
+        const cell4 = row.insertCell(3);
+        const node_btn = document.createElement("button");
+        node_btn.innerHTML = "Verwijder";
+        node_btn.setAttribute("class","productlist__btn");
+        node_btn.setAttribute("onclick","deleteProduct("+data.id+")");
+        cell4.appendChild(node_btn);
+      }, 1000);
+      
+      closeBarcodeModal();
+    });
+}
+
+closeBcModalBtn.onclick = function() {
+  closeBarcodeModal();
+}
+
+window.onclick = function(event) {
+  if (event.target == bcModal) {
+    closeBarcodeModal();
+  }
+}
+
+html5QrcodeScanner.start(
+  { facingMode: "environment"},
+  config,
+  qrCodeSuccessCallback
+);
